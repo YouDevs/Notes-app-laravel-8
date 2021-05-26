d9d585f58a661b3a71adde10a78071aa455d8d6e
carlos.develops <carlos.develops@gmail.com>
2021-05-25 21:05:32 -0500

    conf DB, seeder y listar notas

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\NoteController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::Resource('notas', NoteController::class);
